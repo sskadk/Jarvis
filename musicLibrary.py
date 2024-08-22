1ec49656c26bbcfa236b91736531832533f9700a
Swastik Adhikari <78269644+sskadk@users.noreply.github.com>
2024-08-22 13:48:27 +0545

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -6,3 +6,4 @@ music = {
     "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
     "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
 }
+
